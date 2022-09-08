b74ef6e4f469c582d6d2b3a937462718624146a7
Dkingofcode <doladepo128@gmail.com>
2022-09-08 19:21:48 +0100

    first commit

---
+++ react-user-auth/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './Login';
+import Register from './Register';
+
+
+function App() {
+  return (
+    <div className="App">
+          <h1 className='title'>Learn React</h1>
+          <Login />
+    </div>
+  );
+}
+
+export default App;
